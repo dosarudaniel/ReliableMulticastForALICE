5d13bd22db2ff6b34460451c91fdbc0978fc5a93
dosarudaniel <dosarudaniel@gmail.com>
2019-03-14 08:40:49 +0200

    Improved package description

--- src/myjava/com/github/dosarudaniel/gsoc/package-info.java
+++ src/myjava/com/github/dosarudaniel/gsoc/package-info.java
@@ -1,7 +1,7 @@
 /**
- * myjava.com.github.dosarudaniel.gsoc contains the Blob, Sender and receiver
+ * myjava.com.github.dosarudaniel.gsoc contains the Blob, Sender and Receiver
  * classes used for sending/receiving multicast messages that contains serialized
- * objects with a random length, random content String and a separate field for
- * a checksum of the payload
+ * objects with a random length, random content String and a separate checksum
+ * field
  */
 package myjava.com.github.dosarudaniel.gsoc;
--- src/test/com/github/dosarudaniel/gsoc/package-info.java
+++ src/test/com/github/dosarudaniel/gsoc/package-info.java
@@ -2,6 +2,6 @@
  * test.com.github.dosarudaniel.gsoc contains the TestSender and TestReceiver
  * classes used for testing the ability to send and receive multicast messages
  * that contains serialized objects with a random length, random content String
- * and a separate field for a checksum of the payload
+ * and a separate checksum field of the payload
  */
 package test.com.github.dosarudaniel.gsoc;
