6d8d12d1dd664c85d860e9ab196c3bcabf4b017a
dosarudaniel <dosarudaniel@gmail.com>
2019-05-14 12:32:53 +0300

    Moved generateRandomString to Utils

--- src/myjava/com/github/dosarudaniel/gsoc/Utils.java
+++ src/myjava/com/github/dosarudaniel/gsoc/Utils.java
@@ -4,10 +4,13 @@ import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Utils {
 	public final static int FRAGMENTED_BLOB_HEADER_LENGHT = 43;
 	public final static int PACKET_MAX_SIZE = 65536;
+	
+	static final String AB = "0123456789abcdefghijklmnopqrstuvwxyz";
 
 	public static byte[] calculateChecksum(byte[] data) throws NoSuchAlgorithmException {
 		MessageDigest mDigest = MessageDigest.getInstance("SHA1");
@@ -27,5 +30,21 @@ public class Utils {
 		bb.putLong(uuid.getMostSignificantBits());
 		bb.putLong(uuid.getLeastSignificantBits());
 		return bb.array();
+	}
+	
+	/**
+	 * Generates a random content string of length len
+	 *
+	 * @param len - Length of the randomString
+	 * @return String - A random content string of length len
+	 */
+	public static String generateRandomString(int len) {
+		StringBuilder sb = new StringBuilder(len);
+		for (int i = 0; i < len; i++) {
+			int randomNumber = ThreadLocalRandom.current().nextInt(0, AB.length());
+			sb.append(AB.charAt(randomNumber));
+		}
+
+		return sb.toString();
 	}
 }
--- src/test/com/github/dosarudaniel/gsoc/MulticastServer.java
+++ src/test/com/github/dosarudaniel/gsoc/MulticastServer.java
@@ -14,13 +14,13 @@ import java.util.concurrent.ThreadLocalRandom;
 import myjava.com.github.dosarudaniel.gsoc.Blob;
 import myjava.com.github.dosarudaniel.gsoc.Blob.PACKET_TYPE;
 import myjava.com.github.dosarudaniel.gsoc.FragmentedBlob;
+import myjava.com.github.dosarudaniel.gsoc.Utils;
 
 // TODO, posibil de integrat in Receiver sau Sender
 public class MulticastServer {
 	static final int MIN_LEN = 50;
 	static final int MAX_LEN = 130;
-	static final String AB = "34";
-
+	
 	Map<UUID, Blob> inFlight = new HashMap<>(); // uuid <-> Blob fragmentat, zonă de tranzitie până la primirea completă
 												// a tuturor fragmentelor
 	Map<String, Blob> currentCacheContent = new HashMap<>(); // Blob-uri complete
@@ -31,7 +31,7 @@ public class MulticastServer {
 		int BUF_SIZE = 65536;
 		byte[] buf = new byte[BUF_SIZE];
 
-		String payload = randomString(ThreadLocalRandom.current().nextInt(MIN_LEN, MAX_LEN));
+		String payload = Utils.generateRandomString(ThreadLocalRandom.current().nextInt(MIN_LEN, MAX_LEN));
 		String key = "te";
 		UUID uuid = UUID.randomUUID();
 		FragmentedBlob fBlob = new FragmentedBlob(payload.getBytes(Charset.forName("UTF-8")), 257, PACKET_TYPE.DATA,
@@ -79,19 +79,4 @@ public class MulticastServer {
 	// Thread incompleteBlobRecovery = new Thread();
 	// daca inFlight contine obiecte care nu au fost atinse de mai mult de X millis
 	// => recovery procedure pentru ele
-	/**
-	 * Generates a random content string of length len
-	 *
-	 * @param len - Length of the randomString
-	 * @return String - A random content string of length len
-	 */
-	static String randomString(int len) {
-		StringBuilder sb = new StringBuilder(len);
-		for (int i = 0; i < len; i++) {
-			int randomNumber = ThreadLocalRandom.current().nextInt(0, AB.length());
-			sb.append(AB.charAt(randomNumber));
-		}
-
-		return sb.toString();
-	}
 }
