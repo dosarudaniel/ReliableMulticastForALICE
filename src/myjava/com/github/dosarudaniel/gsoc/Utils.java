d9a605f9b2f7429b759e8cd3b56ca22a917ddcd4
dosarudaniel <dosarudaniel@gmail.com>
2019-05-20 17:12:12 +0300

    Moved sendMulticast method in Utils class

--- src/myjava/com/github/dosarudaniel/gsoc/Utils.java
+++ src/myjava/com/github/dosarudaniel/gsoc/Utils.java
@@ -1,5 +1,9 @@
 package myjava.com.github.dosarudaniel.gsoc;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -36,6 +40,7 @@ public class Utils {
 
 	public final static int SIZE_OF_FRAGMENTED_BLOB_HEADER = SIZE_OF_FRAGMENT_OFFSET + SIZE_OF_PACKET_TYPE
 			+ SIZE_OF_UUID + SIZE_OF_BLOB_PAYLOAD_LENGTH + SIZE_OF_PAYLOAD_CHECKSUM + SIZE_OF_KEY_LENGTH;
+	public final static int SIZE_OF_FRAGMENTED_BLOB_HEADER_AND_TRAILER = SIZE_OF_FRAGMENTED_BLOB_HEADER + SIZE_OF_PACKET_CHECKSUM;
 
 	// Start indexes of the fields in the serialized byte[]
 	public final static int FRAGMENT_OFFSET_START_INDEX = 0;
@@ -86,5 +91,24 @@ public class Utils {
 		}
 
 		return sb.toString();
+	}
+	
+	/**
+	 * Sends multicast message that contains the serialized version of a
+	 * fragmentedBlob
+	 *
+	 * @param packet          - serialized fragmented Blob to send
+	 * @param destinationIp   - Destination IP address (multicast)
+	 * @param destinationPort - Destination port number
+	 * @throws IOException
+	 * @throws NoSuchAlgorithmException
+	 */
+	public static void sendFragmentMulticast(byte[] packet, String destinationIp, int destinationPort)
+			throws IOException, NoSuchAlgorithmException {
+		try (DatagramSocket socket = new DatagramSocket()) {
+			InetAddress group = InetAddress.getByName(destinationIp);
+			DatagramPacket datagramPacket = new DatagramPacket(packet, packet.length, group, destinationPort);
+			socket.send(datagramPacket);
+		}
 	}
 }
