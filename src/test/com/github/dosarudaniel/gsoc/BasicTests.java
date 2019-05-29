9be692de8857d2332ade4dddf69db0335f8ddda3
dosarudaniel <dosarudaniel@gmail.com>
2019-05-29 23:49:36 +0300

    Added simple tests for TreeSet<Pair>

--- src/test/com/github/dosarudaniel/gsoc/BasicTests.java
+++ src/test/com/github/dosarudaniel/gsoc/BasicTests.java
@@ -2,9 +2,13 @@ package test.com.github.dosarudaniel.gsoc;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.TreeSet;
 
 import myjava.com.github.dosarudaniel.gsoc.Utils;
+import myjava.com.github.dosarudaniel.gsoc.Utils.Pair;
+import myjava.com.github.dosarudaniel.gsoc.Utils.PairComparator;
 
 public class BasicTests {
 
@@ -29,6 +33,37 @@ public class BasicTests {
 	for (Map.Entry<String, String> entry : metadataMap2.entrySet()) {
 	    System.out.println("\t" + entry.getKey() + " : " + entry.getValue());
 	}
+
+	// Test the TreeSet
+	TreeSet<Pair> treeSet = new TreeSet<>(new PairComparator());
+
+	Pair p1 = new Pair(100, 160);
+	Pair p2 = new Pair(210, 400);
+	Pair p3 = new Pair(0, 100);
+	Pair p4 = new Pair(400, 430);
+	Pair p5 = new Pair(160, 210);
+
+	treeSet.add(p1);
+	System.out.println(treeSet);
+	treeSet.add(p2);
+	System.out.println(treeSet);
+	treeSet.add(p3);
+
+	System.out.println(" ");
+	Iterator<Pair> iterator = treeSet.iterator();
+	while (iterator.hasNext())
+	    System.out.print(iterator.next());
+	System.out.println(" ");
+
+	System.out.println(treeSet);
+	treeSet.add(p4);
+	System.out.println(treeSet);
+	treeSet.add(p5);
+	System.out.println(treeSet);
+
+	byte[] test = new byte[10];
+	System.out.println(test.length);
+
     }
 
 }
