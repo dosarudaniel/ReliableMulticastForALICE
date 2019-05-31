2d4423e93364b161f3115deb722abdd24389826e
dosarudaniel <dosarudaniel@gmail.com>
2019-05-31 13:53:14 +0300

    Added description for each net.core.<parameter>

--- kernel_configuration.sh
+++ kernel_configuration.sh
@@ -1,3 +1,11 @@
+# The maximum socket receive buffer size which may be set by using the SO_RCVBUF socket option:
 sysctl -w net.core.rmem_max=26214400
+
+# The default setting in bytes of the socket receive buffer:
 sysctl -w net.core.rmem_default=26214400
+
+# The kernel parameter “netdev_max_backlog” is the maximum size of the receive queue. 
+# The received frames will be stored in this queue after taking them from the ring buffer on the NIC.
+# Use high value for high speed cards to prevent loosing packets.
 sysctl -w net.core.netdev_max_backlog=250000
+
