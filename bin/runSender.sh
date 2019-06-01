2623863531b648f7b61d8ac3a063da878b075756
dosarudaniel <dosarudaniel@gmail.com>
2019-06-01 12:03:59 +0300

    Update runSender.sh

--- bin/runSender.sh
+++ bin/runSender.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 function usage {
-    echo "usage: $0 <MULTICAST_IP_ADDRESS> <PORT_NUMBER>"
+    echo "usage: $0 <MULTICAST_IP_ADDRESS> <PORT_NUMBER> <FRAGMENT_MAX_PAYLOAD_SIZE> <KEY_LENGTH> <METADATA_LENGTH> <PAYLOAD_LENGTH> <NR_OF_PACKETS_TO_BE_SENT>"
     exit 1
 }
 
-if [[ $# -ne 2 ]]; then
+if [[ $# -ne 7 ]]; then
   usage
 else
-  java -cp . test.com.github.dosarudaniel.gsoc.TestSender $1 $2
+  java -cp . test.com.github.dosarudaniel.gsoc.TestSender $1 $2 $3 $4 $5 $6 $7
 fi
