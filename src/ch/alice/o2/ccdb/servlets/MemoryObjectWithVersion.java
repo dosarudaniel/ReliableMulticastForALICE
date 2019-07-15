49b637f33bea9ee07a09d48161af867c7b7254f6
dosarudaniel <dosarudaniel@gmail.com>
2019-07-16 00:00:31 +0300

    Added blob member in MemoryObjectWithVersion

--- src/ch/alice/o2/ccdb/servlets/MemoryObjectWithVersion.java
+++ src/ch/alice/o2/ccdb/servlets/MemoryObjectWithVersion.java
@@ -2,11 +2,14 @@ package ch.alice.o2.ccdb.servlets;
 
 import java.io.File;
 
-public class MemoryObjectWithVersion extends LocalObjectWithVersion {
+import myjava.com.github.dosarudaniel.gsoc.Blob;
 
-    public MemoryObjectWithVersion(long startTime, File entry) {
+public class MemoryObjectWithVersion extends LocalObjectWithVersion {
+    private Blob blob;
+
+    public MemoryObjectWithVersion(long startTime, File entry, Blob blob) {
 	super(startTime, entry);
-	// TODO Auto-generated constructor stub
+	this.blob = blob;
     }
 
 }
